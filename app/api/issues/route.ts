a524abf9cd11b1abf1cee8cc51d0c151700ed6fb
KawalaE <edyta.wer.kawala@gmail.com>
2024-08-22 14:06:50 +0200

    Build API fro creating issues

---
+++ app/api/issues/route.ts
@@ -0,0 +1,25 @@
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const postRequestSchema = z.object({
+  title: z.string().min(1).max(256),
+  description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validate = postRequestSchema.safeParse(body);
+
+  if (!validate.success) {
+    return NextResponse.json(validate.error.errors, { status: 400 });
+  }
+
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+  return NextResponse.json(newIssue, { status: 201 });
+}
