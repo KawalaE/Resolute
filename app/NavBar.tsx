8440d49f4d0e478b8dbc57d8a0bcdf69f49e0a6d
KawalaE <edyta.wer.kawala@gmail.com>
2024-08-22 12:31:23 +0200

    Style the active links in the navigation

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,11 +1,17 @@
+"use client";
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { GrBug } from "react-icons/gr";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const navElements = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+
   return (
     <nav className="flex items-center space-x-6 border-b mb-5 px-5 h-14">
       <Link href="/">
@@ -17,7 +23,11 @@ const NavBar = () => {
             <Link
               key={link.href}
               href={link.href}
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              className={classNames({
+                "text-zinc-500": currentPath !== link.href,
+                "text-zinc-900": currentPath === link.href,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
             >
               {link.label}
             </Link>
