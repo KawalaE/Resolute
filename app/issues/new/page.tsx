761ffae8b5405ff33a9cf114c721662e5cc7c8ea
KawalaE <edyta.wer.kawala@gmail.com>
2024-08-23 12:25:21 +0200

    Create form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,37 @@
 "use client";
 import { Button, TextField } from "@radix-ui/themes";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+import { Controller, useForm } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
 
+interface NewIssueData {
+  title: string;
+  description: string;
+}
+
 const NewIssuePage = () => {
+  const router = useRouter();
+  const submitFormData = async (data: NewIssueData) => {
+    await axios.post("/api/issues", data);
+    router.push("/issues");
+  };
+
+  const { register, control, handleSubmit } = useForm<NewIssueData>();
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title"></TextField.Root>
-      <SimpleMDE placeholder="Description" />
+    <form
+      className="max-w-xl space-y-5"
+      onSubmit={handleSubmit((data) => submitFormData(data))}
+    >
+      <TextField.Root placeholder="Title" {...register("title")} />
+      <Controller
+        control={control}
+        name="description"
+        render={({ field }) => <SimpleMDE {...field} />}
+      />
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme accentColor="teal" grayColor="gray">
+        <Theme accentColor="indigo" grayColor="gray">
           <NavBar />
           <main className="p-5">{children}</main>
         </Theme>
