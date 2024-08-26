16e91ccd0f1730fbe95ad8c5659df66fcf7e5fa7
KawalaE <edyta.wer.kawala@gmail.com>
2024-08-26 19:42:46 +0200

    Show confirmation dialog

--- app/issues/[id]/DeleteIssuse.tsx
+++ app/issues/[id]/DeleteIssuse.tsx
@@ -1,12 +1,36 @@
 import { TrashIcon } from "@radix-ui/react-icons";
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 
 const DeleteIssuse = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color="red" className="p-3">
-      <TrashIcon />
-      Delete an issue
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red" className="p-3">
+          <TrashIcon />
+          Delete an issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Delete an Issue</AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          Are you sure? This action will permanently remove an issue and any
+          data related to it.
+        </AlertDialog.Description>
+
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              Delete an issue
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
