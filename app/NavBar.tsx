3123de45e89f961a38a7935adc7594a675491a71
KawalaE <edyta.wer.kawala@gmail.com>
2024-08-27 11:19:16 +0200

    Add the login and logout buttons

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,15 +1,19 @@
 "use client";
 import classNames from "classnames";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { GrBug } from "react-icons/gr";
 
 const NavBar = () => {
   const currentPath = usePathname();
-
+  const { status, data: user } = useSession();
   const navElements = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
+    status === "authenticated"
+      ? { label: "Logout", href: "/api/auth/signout" }
+      : { label: "Login", href: "/api/auth/signin" },
   ];
 
   return (
@@ -20,17 +24,18 @@ const NavBar = () => {
       <ul className="flex space-x-6">
         {navElements.map((link) => {
           return (
-            <Link
-              key={link.href}
-              href={link.href}
-              className={classNames({
-                "text-zinc-500": currentPath !== link.href,
-                "text-zinc-900": currentPath === link.href,
-                "hover:text-zinc-800 transition-colors": true,
-              })}
-            >
-              {link.label}
-            </Link>
+            <li key={link.href}>
+              <Link
+                href={link.href}
+                className={classNames({
+                  "text-zinc-500": currentPath !== link.href,
+                  "text-zinc-900": currentPath === link.href,
+                  "hover:text-zinc-800 transition-colors": true,
+                })}
+              >
+                {link.label}
+              </Link>
+            </li>
           );
         })}
       </ul>
