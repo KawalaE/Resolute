9c7d7c4b77e0400a6130ea42479ebdb0b25c576a
KawalaE <edyta.wer.kawala@gmail.com>
2024-09-06 18:54:38 +0200

    Isolate Issues Selector component

--- app/issues/IssuesFilter.tsx
+++ app/issues/IssuesFilter.tsx
@@ -1,17 +1,14 @@
 "use client";
-import { Priority, Status } from "@prisma/client";
-import { Flex, Select } from "@radix-ui/themes";
-import { useRouter, useSearchParams } from "next/navigation";
-import { IssueBadge } from "../components";
-import { PriorityBadge } from "../components/PriorityBadge";
+import { Flex } from "@radix-ui/themes";
+import IssuesSelector, { PrioritiesArr, StatusArr } from "./IssuesSelector";
 
-const statuses: { label: string; value?: Status }[] = [
+const statuses: StatusArr = [
   { label: "all" },
   { label: "open", value: "OPEN" },
   { label: "closed", value: "CLOSED" },
   { label: "in progress", value: "IN_PROGRESS" },
 ];
-const priorities: { label: string; value?: Priority }[] = [
+const priorities: PrioritiesArr = [
   { label: "all" },
   { label: "low", value: "LOW" },
   { label: "medium", value: "MEDIUM" },
@@ -21,75 +18,19 @@ const priorities: { label: string; value?: Priority }[] = [
 const IssuesFilter = () => {
   return (
     <Flex gap="5">
-      <StatusSelector />
-      <PrioritySelector />
+      <IssuesSelector
+        selectBy="status"
+        secondarySelector="priority"
+        options={statuses}
+      />
+
+      <IssuesSelector
+        selectBy="priority"
+        secondarySelector="status"
+        options={priorities}
+      />
     </Flex>
   );
 };
 
-const PrioritySelector = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  return (
-    <Select.Root
-      defaultValue={searchParams.get("priority") || ""}
-      onValueChange={(priority) => {
-        const params = new URLSearchParams();
-        if (priority) params.append("priority", priority);
-        if (searchParams.get("orderBy"))
-          params.append("orderBy", searchParams.get("orderBy")!);
-        if (searchParams.get("status"))
-          params.append("status", searchParams.get("status")!);
-        const query = params.size ? "?" + params.toString() : "";
-        router.push("/issues" + query);
-      }}
-    >
-      <Select.Trigger placeholder="Filter by priority..." />
-      <Select.Content variant="soft">
-        {priorities.map((priority) => (
-          <Select.Item key={priority.label} value={priority?.value || "all"}>
-            {priority.label === "all" ? (
-              "all priorities"
-            ) : (
-              <PriorityBadge priority={priority.value!} />
-            )}
-          </Select.Item>
-        ))}
-      </Select.Content>
-    </Select.Root>
-  );
-};
-
-const StatusSelector = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  return (
-    <Select.Root
-      defaultValue={searchParams.get("status") || ""}
-      onValueChange={(status) => {
-        const params = new URLSearchParams();
-        if (status) params.append("status", status);
-        if (searchParams.get("orderBy"))
-          params.append("orderBy", searchParams.get("orderBy")!);
-        if (searchParams.get("priority"))
-          params.append("priority", searchParams.get("priority")!);
-        const query = params.size ? "?" + params.toString() : "";
-        router.push("/issues" + query);
-      }}
-    >
-      <Select.Trigger placeholder="Filter by status..." />
-      <Select.Content variant="soft">
-        {statuses.map((status) => (
-          <Select.Item key={status.label} value={status?.value || "all"}>
-            {status.label === "all" ? (
-              "all issues"
-            ) : (
-              <IssueBadge status={status.value!} />
-            )}
-          </Select.Item>
-        ))}
-      </Select.Content>
-    </Select.Root>
-  );
-};
 export default IssuesFilter;
---
+++ app/issues/IssuesSelector.tsx
@@ -0,0 +1,53 @@
+import { Priority, Status } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
+import { IssueBadge } from "../components";
+import { PriorityBadge } from "../components/PriorityBadge";
+
+export type StatusArr = { label: string; value?: Status }[];
+export type PrioritiesArr = { label: string; value?: Priority }[];
+
+interface Props {
+  selectBy: "priority" | "status";
+  secondarySelector: "priority" | "status";
+  options: StatusArr | PrioritiesArr;
+}
+const IssuesSelector = ({ selectBy, secondarySelector, options }: Props) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  return (
+    <Select.Root
+      defaultValue={searchParams.get(selectBy) || ""}
+      onValueChange={(selected) => {
+        const params = new URLSearchParams();
+        if (selected) params.append(selectBy, selected);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
+        if (searchParams.get(secondarySelector))
+          params.append(
+            secondarySelector,
+            searchParams.get(secondarySelector)!
+          );
+        const query = params.size ? "?" + params.toString() : "";
+        router.push("/issues" + query);
+      }}
+    >
+      <Select.Trigger placeholder={`Filter by ${selectBy}...`} />
+      <Select.Content variant="soft">
+        {options.map((option) => (
+          <Select.Item key={option.label} value={option?.value || "all"}>
+            {option.label === "all" ? (
+              "all"
+            ) : selectBy === "status" ? (
+              <IssueBadge status={option.value!} />
+            ) : (
+              <PriorityBadge priority={option.value!} />
+            )}
+          </Select.Item>
+        ))}
+      </Select.Content>
+    </Select.Root>
+  );
+};
+
+export default IssuesSelector;
