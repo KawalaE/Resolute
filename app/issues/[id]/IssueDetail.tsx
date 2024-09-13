2ef44ed921770294b19f8c72d99f849f5d81e996
KawalaE <edyta.wer.kawala@gmail.com>
2024-09-13 19:32:53 +0200

    Display the priority on the edit page of an issue

--- app/issues/[id]/IssueDetail.tsx
+++ app/issues/[id]/IssueDetail.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { IssueBadge } from "@/app/components";
+import { PriorityBadge } from "@/app/components/PriorityBadge";
 import { Issue } from "@prisma/client";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import ReactMarkdown from "react-markdown";
@@ -10,6 +11,7 @@ const IssueDetail = ({ issue }: { issue: Issue }) => {
       <Heading>{issue.title}</Heading>
       <Flex gap="4" my="2">
         <IssueBadge status={issue.status} />
+        <PriorityBadge priority={issue.priority} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
       <Card className="prose max-w-full dark:markdown-dark">
