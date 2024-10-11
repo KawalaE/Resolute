e66c48c65432014d9e56f5e085b72125a619a02e
KawalaE <edyta.wer.kawala@gmail.com>
2024-10-11 18:47:34 +0200

    Add metadata to New issue page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import dynamic from "next/dynamic";
 import IssueFormSkeleton from "./loading";
 
@@ -11,6 +12,12 @@ const IssueForm = dynamic(
 
 const NewIssuePage = () => {
   return <IssueForm />;
+};
+
+export const metadata: Metadata = {
+  title: "Resolute - New",
+  description: `Create a new issue, set the priority, add a title,
+   and include code snippets for a detailed description.`,
 };
 
 export default NewIssuePage;
