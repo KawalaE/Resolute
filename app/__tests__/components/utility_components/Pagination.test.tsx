0e96c03c530098af5aec98409ee7fb10ede77793
KawalaE <edyta.wer.kawala@gmail.com>
2024-10-16 13:06:42 +0200

    Add router tests to the Pagination component

--- app/__tests__/components/utility_components/Pagination.test.tsx
+++ app/__tests__/components/utility_components/Pagination.test.tsx
@@ -1,5 +1,7 @@
 import Pagination from "@/app/_utility_components/Pagination";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import mockRouter from "next-router-mock";
 import { describe, expect, it } from "vitest";
 
 describe("Pagination", () => {
@@ -50,6 +52,28 @@ describe("Pagination", () => {
     expect(leftBtn).not.toBeDisabled();
     expect(doubleLeftBtn).not.toBeDisabled();
   });
+  it("should change url query, to previous page on click - left button", async () => {
+    mockRouter.push("/issues?page=2");
+    renderComponent(30, 5, 2);
+    const leftBtn = screen.getByLabelText("left");
+
+    const user = userEvent.setup();
+    await user.click(leftBtn);
+
+    expect(mockRouter.asPath).toContain("page=1");
+    expect(mockRouter.asPath).not.toContain("page=2");
+  });
+  it("should change url query, to the first page on click - double left button", async () => {
+    mockRouter.push("/issues?page=5");
+    renderComponent(30, 5, 2);
+    const doubleLeftBtn = screen.getByLabelText("double-left");
+
+    const user = userEvent.setup();
+    await user.click(doubleLeftBtn);
+
+    expect(mockRouter.asPath).toContain("page=1");
+    expect(mockRouter.asPath).not.toContain("page=5");
+  });
   it("should be disabled while on last page - right button/double right", () => {
     renderComponent(30, 5, 6);
 
@@ -67,5 +91,27 @@ describe("Pagination", () => {
 
     expect(leftBtn).not.toBeDisabled();
     expect(doubleLeftBtn).not.toBeDisabled();
+  });
+  it("should change url query, to the next page on click - right button", async () => {
+    mockRouter.push("/issues?page=2");
+    renderComponent(30, 5, 2);
+    const rightBtn = screen.getByLabelText("right");
+
+    const user = userEvent.setup();
+    await user.click(rightBtn);
+
+    expect(mockRouter.asPath).toContain("page=3");
+    expect(mockRouter.asPath).not.toContain("page=2");
+  });
+  it("should change url query, to the last page on click - double right button", async () => {
+    mockRouter.push("/issues?page=2");
+    renderComponent(30, 5, 2);
+    const doubleRightBtn = screen.getByLabelText("double-right");
+
+    const user = userEvent.setup();
+    await user.click(doubleRightBtn);
+
+    expect(mockRouter.asPath).toContain("page=6");
+    expect(mockRouter.asPath).not.toContain("page=2");
   });
 });
