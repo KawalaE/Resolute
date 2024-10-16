39cecef61a7fce8baf71d58eb387230ff2d56464
KawalaE <edyta.wer.kawala@gmail.com>
2024-10-16 15:21:57 +0200

    Add router tests to the IssueSelector

--- app/__tests__/components/issues/IssuesSelector.test.tsx
+++ app/__tests__/components/issues/IssuesSelector.test.tsx
@@ -5,8 +5,8 @@ import { Theme } from "@radix-ui/themes";
 import "@testing-library/jest-dom/vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import mockRouter from "next-router-mock";
 import { describe, expect, it } from "vitest";
-
 const statuses: StatusArr = [
   { label: "open", value: "OPEN" },
   { label: "closed", value: "CLOSED" },
@@ -39,6 +39,22 @@ describe("IssuesSelector", () => {
 
     statuses.forEach((status) => {
       expect(screen.getByText(status.label)).toBeInTheDocument();
+    });
+  });
+  it("should modify url queries to contain current status", async () => {
+    mockRouter.push("/issues");
+    renderComponent();
+
+    const selector = screen.getByRole("combobox", { hidden: true });
+    const user = userEvent.setup();
+
+    statuses.forEach(async (status) => {
+      await user.click(selector);
+
+      const issuesPerPageBtn = await screen.findByText(status.label);
+      await user.click(issuesPerPageBtn);
+
+      expect(mockRouter.asPath).toContain(`status=${status.value}`);
     });
   });
 });
