d2bbb72eb4a6be2fd81cc48e366d4f25add78933
KawalaE <edyta.wer.kawala@gmail.com>
2024-10-16 12:23:26 +0200

    Add route tests to the Link component

--- app/__tests__/components/issues/ResetFilters.test.tsx
+++ app/__tests__/components/issues/ResetFilters.test.tsx
@@ -1,11 +1,16 @@
 import ResetFilters from "@/app/issues/_components/ResetFilters";
 import "@testing-library/jest-dom/vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import mockRouter from "next-router-mock";
+import { MemoryRouterProvider } from "next-router-mock/MemoryRouterProvider";
 import { describe, expect, it } from "vitest";
 
 describe("ResetFilters", () => {
   const renderComponent = () => {
-    render(<ResetFilters reset={false} resetHandler={() => {}} />);
+    render(<ResetFilters reset={false} resetHandler={() => {}} />, {
+      wrapper: MemoryRouterProvider,
+    });
   };
   it("should render a button component", () => {
     renderComponent();
@@ -16,5 +21,16 @@ describe("ResetFilters", () => {
     renderComponent();
 
     expect(screen.getByText(/reset/i)).toBeInTheDocument();
+  });
+  it("should reset the path by removing additional queries from url", async () => {
+    renderComponent();
+    //simulate a path with additional queries
+    mockRouter.push("/issues?count=5&priority=MEDIUM&status=CLOSED");
+
+    const resetBtn = screen.getByText(/reset/i);
+    const user = userEvent.setup();
+    await user.click(resetBtn);
+
+    expect(mockRouter.asPath).toEqual("/issues");
   });
 });
