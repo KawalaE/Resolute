e476565132a0fee69708bf0e60b20e57af10edac
KawalaE <edyta.wer.kawala@gmail.com>
2024-10-16 16:08:08 +0200

    Test the chcange of order in Issue Table headers

--- app/__tests__/components/issues/IssueTable.test.tsx
+++ app/__tests__/components/issues/IssueTable.test.tsx
@@ -1,10 +1,13 @@
 import IssueTable from "@/app/issues/_components/IssueTable";
 import "@testing-library/jest-dom/vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import mockRouter from "next-router-mock";
 import { describe, expect, it } from "vitest";
 import { data } from "../../__mocks__/dataBaseMock";
 
 describe("IssueTable", () => {
+  const headers = ["title", "status", "priority", "created"];
   it("should display a table with issues", () => {
     render(<IssueTable searchParams={{}} issues={data} />);
 
@@ -12,7 +15,6 @@ describe("IssueTable", () => {
   });
   it("should display correct headers", () => {
     render(<IssueTable searchParams={{}} issues={data} />);
-    const headers = ["title", "status", "priority", "created"];
 
     headers.forEach((header) => {
       expect(screen.getByText(new RegExp(header, "i"))).toBeInTheDocument();
@@ -33,5 +35,23 @@ describe("IssueTable", () => {
         screen.getByText(new RegExp(issue.createdAt.toDateString(), "i"))
       ).toBeInTheDocument();
     });
+  });
+  it("it should change the url according to the headers clicked", async () => {
+    mockRouter.push("/issues");
+    render(<IssueTable searchParams={{}} issues={data} />);
+
+    for (const header of headers) {
+      const actualHeader = screen.getByText(new RegExp(header, "i"));
+
+      const user = userEvent.setup();
+      await user.click(actualHeader);
+
+      expect(mockRouter.asPath).toContain(`orderBy=${header}`);
+      expect(mockRouter.asPath).toContain("sort=asc");
+
+      await user.click(actualHeader);
+
+      expect(mockRouter.asPath).toContain("sort=desc");
+    }
   });
 });
