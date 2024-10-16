2ca0dfa92521e19b45a19032ef718c6e4b8e16f2
KawalaE <edyta.wer.kawala@gmail.com>
2024-10-16 14:08:38 +0200

    Add router tests to the IssuesPerPageSelector

--- app/__tests__/components/issues/IssuesPerPageSelector.test.tsx
+++ app/__tests__/components/issues/IssuesPerPageSelector.test.tsx
@@ -3,7 +3,9 @@ import { Theme } from "@radix-ui/themes";
 import "@testing-library/jest-dom/vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import mockRouter from "next-router-mock";
 import { describe, expect, it } from "vitest";
+
 describe("IssuesPerPageSelector", () => {
   const renderComponent = () => {
     render(<IssuesPerPageSelector reset={false} resetHandler={() => {}} />, {
@@ -24,6 +26,22 @@ describe("IssuesPerPageSelector", () => {
 
     [5, 10, 15, 20].forEach((amount) => {
       expect(screen.getByText(amount)).toBeInTheDocument();
+    });
+  });
+  it("should modify url queries to contain count of pages", async () => {
+    mockRouter.push("/issues");
+    renderComponent();
+
+    const selector = screen.getByRole("combobox", { hidden: true });
+    const user = userEvent.setup();
+
+    [5, 10, 15, 20].forEach(async (amount) => {
+      await user.click(selector);
+
+      const issuesPerPageBtn = await screen.findByText(amount);
+      await user.click(issuesPerPageBtn);
+
+      expect(mockRouter.asPath).toContain(`count=${amount}`);
     });
   });
 });
